Fixed gradient

image_decoder.py
@@ -5,34 +5,28 @@ class ImageDecoder(tf.keras.Model):
     def __init__(self, embedding_dim):
         super(ImageDecoder, self).__init__()
         # shape after fc == (batch_size, 64, embedding_dim)
-        self.decode_fc1 = tf.keras.layers.Dense(embedding_dim, activation='relu', name='decode_fc1')
-        self.decode_fc2 = tf.keras.layers.Dense(2048, activation='relu', name='decode_fc2')
-        self.reshape = tf.keras.layers.Reshape((8,8,2048))
-        self.deconv1 = tf.keras.layers.Conv2DTranspose(filters=512, kernel_size=(3,3), activation='relu', padding='valid', strides=(2,2), name='deconv1')
-        self.deconv2 = tf.keras.layers.Conv2DTranspose(filters=256, kernel_size=(3,3), activation='relu', padding='valid', strides=(2,2), name='deconv2')
-        self.deconv3 = tf.keras.layers.Conv2DTranspose(filters=64, kernel_size=(5,5), activation='relu', padding='valid', strides=(2,2), name='deconv3')
-        self.deconv4 = tf.keras.layers.Conv2DTranspose(filters=16, kernel_size=(4,4), activation='relu', padding='valid', strides=(2,2), name='deconv4')
-        self.deconv5 = tf.keras.layers.Conv2DTranspose(filters=3, kernel_size=(5,5), activation='sigmoid', padding='valid', strides=(2,2), name='deconv5')
+        self.reshape = tf.keras.layers.Reshape((8,8,embedding_dim))
+        self.deconv1 = tf.keras.layers.Conv2D(128, (3,3), padding='same')# 8x8
+        self.upsample1 = tf.keras.layers.UpSampling2D((3,3))# 24x24
+        self.deconv2 = tf.keras.layers.Conv2D(64, (5,5), padding='valid') # 20x20
+        self.upsample2 = tf.keras.layers.UpSampling2D((3,3)) # 60x60
+        self.deconv3 = tf.keras.layers.Conv2D(16, (5,5), padding='same') # 56x56
+        self.upsample3 = tf.keras.layers.UpSampling2D((3,3)) # 168x168
+        self.deconv4 = tf.keras.layers.Conv2D(3, (5,5), padding='same') # 164x164
+        self.upsample4 = tf.keras.layers.UpSampling2D((2,2)) # 168x168
+        self.crop = tf.keras.layers.Cropping2D(cropping=((15, 15), (14, 14)))
 
-        self.dropout = tf.keras.layers.Dropout(0.2)
-        # self.batch_norm = tf.keras.layers.BatchNormalization()
-
-    def call(self, x, training=False):
-        x = self.decode_fc1(x)
-        if training:
-            x = self.dropout(x)
-        x = self.decode_fc2(x)
-        if training:
-            x = self.dropout(x)
+    def call(self, x):
         x = self.reshape(x)
         x = self.deconv1(x)
+        x = self.upsample1(x)
         # x = self.batch_norm(x)
         x = self.deconv2(x)
+        x = self.upsample2(x)
         # x = self.batch_norm(x)
         x = self.deconv3(x)
-        # x = self.batch_norm(x)
+        x = self.upsample3(x)
         x = self.deconv4(x)
-        # x = self.batch_norm(x)
-        x = self.deconv5(x)
-
+        x = self.upsample4(x)
+        x = self.crop(x)
         return x

irenet.py
@@ -71,7 +71,7 @@ def train(model, dataset, tokenizer, batch_size=16, epochs=20):
                 latent_loss = model.latent_loss(features, features2) / feature_size
             
                 features_out = np.mean([features, features2], axis=0)
-
+                print("loss calc in time {}, {}".format(time.time()-start, latent_loss))
                 img_size = np.float32(1.0)
                 img_shape = img.shape
                 for dim in img_shape:
@@ -79,7 +79,7 @@ def train(model, dataset, tokenizer, batch_size=16, epochs=20):
                 recon = model.image_decoder(features_out)
                 recon_loss = model.img_loss(img, recon) / img_size
 
-                print("recon in time {}".format(time.time()-start))
+                print("recon in time {}, {}".format(time.time()-start, recon_loss))
                 for i in range(1, target.shape[1]):
                     # passing the features through the decoder
                     predictions, hidden, _ = model.text_decoder(dec_input, features_out, hidden)
@@ -89,18 +89,21 @@ def train(model, dataset, tokenizer, batch_size=16, epochs=20):
                     # using teacher forcing
                     dec_input = tf.expand_dims(target[:, i], 1)
             
-            text_loss = loss / int(target.shape[1])
-            final_loss = text_loss + latent_loss + recon_loss
-
+                text_loss = loss / int(target.shape[1])
+                final_loss = text_loss + latent_loss + recon_loss
+            
+            print("RNN in time {} {}".format(time.time()-start, text_loss))
             total_loss += final_loss
             
             variables = model.image_encoder.variables + model.text_encoder.variables + model.text_decoder.variables + model.image_decoder.variables
-            
+            # print(variables)
             gradients = tape.gradient(final_loss, variables) 
-            
+            print("Gradient in time {}".format(time.time()-start))
+            # print(gradients)
             optimizer.apply_gradients(zip(gradients, variables), tf.train.get_or_create_global_step())
+            print("Apply grad in time {}".format(time.time()-start))
             
-            if batch % 100 == 0:
+            if batch % 1 == 0:
                 print ('Epoch {} Batch {} Latent Loss {:.4f} Recon Loss {:.4f} Text Loss {:.4f} Total Loss {:.4f}'.format(epoch + 1, 
                                                             batch, 
                                                             latent_loss.numpy(), recon_loss.numpy(), text_loss.numpy(), final_loss.numpy()))